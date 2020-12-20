01213f61f563cc2c024524e3ff32c8a4bcf39405
darkdanteneo <dark.dante.neo@gmail.com>
2020-12-20 15:32:45 +0530

    init

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+    app.use(express.static('build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '..', 'index.html'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
   }
 
