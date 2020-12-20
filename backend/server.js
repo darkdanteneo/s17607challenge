81929ca1cd3011c830bfbae3c6a469e3cf63d03b
darkdanteneo <dark.dante.neo@gmail.com>
2020-12-20 15:12:44 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', 'src', 'index.html'));
+    });
+  }
 
 mongoose.connect(ur, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
